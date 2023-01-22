8bb79d96b15ff3ebea3941353e2386eddf6453ed
dantelentsoe <dllentsoe@gmail.com>
2023-01-22 07:01:11 +0200

    feat: add color eye dropper

---
+++ client/src/lib/Components/EyeDropper/index.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Palette } from '@untemps/svelte-palette'
+
+  const colors = ['#865C54', '#8F5447', '#A65846', '#A9715E', '#AD8C72']
+
+  let bgColor = colors[0]
+</script>
+
+<main class="flex align-center justify-center mt-48">
+  <Palette
+    {colors}
+    allowDuplicates
+    deletionMode="tooltip"
+    on:select={({ detail: { color } }) => (bgColor = color)} />
+</main>
